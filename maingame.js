8e3a27a33181ae9bc9103efddbe07a97502f2ee6
Annay0510 <annayoungwork@gmail.com>
2022-05-05 16:14:23 -0400

    update

--- maingame.js
+++ maingame.js
@@ -1,7 +1,3 @@
-/*prompt("hi")
-const prompt1=()=>{
-
-}*/
 function promptMe(){
     var welcome= prompt("Welcome to Treasure Hunt! What is your name?");
     alert ("Hey "+welcome+"! Click on the number one to begin the hunt!")
@@ -12,5 +8,43 @@ function fstQuest(){
     if(numOne==="A"){
         alert ("You're answer is true! For the next round, just click on the 2!")}
 
-        else alert ("Oh no! You missed it! No treaure for you!")
+            else alert ("No treaure for you!")
     }
+
+function sndQuest(){
+        var numTwo= prompt("Quest 1 - Solve the riddle: Neat and in order...you get the gist. This tag is used to number your list. Is it A <ol> or B <numli>?")
+        if(numTwo==="A"){
+            alert ("You're answer is true! For the next round, just click on the 2!")}
+    
+                else alert ("Oh no! You missed it! No treaure for you!")
+        }
+
+function trdQuest(){
+        var numThree= prompt("Quest 1 - Solve the riddle: Neat and in order...you get the gist. This tag is used to number your list. Is it A <ol> or B <numli>?")
+        if(numThree==="A"){
+            alert ("You're answer is true! For the next round, just click on the 2!")}
+        
+                else alert ("Oh no! You missed it! No treaure for you!")
+            }
+
+function frthQuest(){
+     var numFour= prompt("Quest 1 - Solve the riddle: Neat and in order...you get the gist. This tag is used to number your list. Is it A <ol> or B <numli>?")
+        if(numFour==="A"){
+            alert ("You're answer is true! For the next round, just click on the 2!")}
+            
+                else alert ("Oh no! You missed it! No treaure for you!")
+                }
+function fithQuest(){
+     var numFive= prompt("Quest 1 - Solve the riddle: Neat and in order...you get the gist. This tag is used to number your list. Is it A <ol> or B <numli>?")
+        if(numFive==="A"){
+            alert ("You're answer is true! For the next round, just click on the 2!")}
+                
+                else alert ("Oh no! You missed it! No treaure for you!")
+                    }
+
+function endQuest(){
+    var goodBye= prompt("Quest 1 - Solve the riddle: Neat and in order...you get the gist. This tag is used to number your list. Is it A <ol> or B <numli>?")
+        if(goodBye==="A"){
+            alert ("You're answer is true! For the next round, just click on the 2!")}
+                                   
+                else alert ("Oh no! You missed it! No treaure for you!")

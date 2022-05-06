6350ce8acc56d38b90f7f34dff449c974a1f43e0
Annay0510 <annayoungwork@gmail.com>
2022-05-06 10:51:05 -0400

    update

--- maingame.js
+++ maingame.js
@@ -11,51 +11,55 @@ function fstQuest(){
     var numOne= prompt("Quest 1 - Solve the riddle: Neat and in order...you get the gist. This tag is used to number your list. Is it A <ol> or B <numli>?")
     if(numOne==="A"){
         alert ("You're answer is true! For the next round, just click on the 2!")
+    }else{
+        let div=document.getElementById ("one").parentElement 
+        let img=document.createElement("img")
+        img.src="pirate.gif"
+        div.append(img)
+        alert ("Oh well...you tried. Today's not your day. The pirate is coming to chase you away! GAME OVER!")
     }
-            else {alert ("Oh well...you tried. Today's not your day. The pirate is coming to chase you away! GAME OVER!")
-        }
     
 
 }
 function sndQuest(){
-        var numTwo= prompt("Quest 2 - Solve the riddle: So many lines- so much at stake. This tag is used to add a line break. Is it A <break> or B <br>?")
-        if(numTwo==="B"){
-            alert ("You're alright with me! For the next round, just click on the 3!")
-        }
-    
-                else {alert ("Oh no! You missed it! Your chances are through. The pirate is coming! No treaure for you! GAME OVER!")
-            }
+    var numTwo= prompt("Quest 2 - Solve the riddle: So many lines- so much at stake. This tag is used to add a line break. Is it A <break> or B <br>?")
+    if(numTwo==="B"){
+        alert ("You're alright with me! For the next round, just click on the 3!")
+    }else{
+        let div=document.getElementById ("two").parentElement 
+        let img=document.createElement("img")
+        img.src="pirate.gif"
+        div.append(img)
+        alert ("Oh no! You missed it! Your chances are through. The pirate is coming! No treaure for you! GAME OVER!")
+    }
  }
 
 function trdQuest(){
-        var numThree= prompt("Quest 3 - Solve the riddle: I worked so hard to make this website. This tag is where I display copywrite. Is it A <footer> or B <copy>?")
-        if(numThree==="A"){
-            alert ("You're ready for more! For the next round, just click on the 4!")
-        }else{
-            let div=document.getElementById ("three").parentElement 
-            let img=document.createElement("img")
-            img.src="pirate.gif"
-            div.append(img)
-            alert ("Oh well...you tried. Today's not your day. The pirate is coming to chase you away! GAME OVER!")
-        }
+    var numThree= prompt("Quest 3 - Solve the riddle: I worked so hard to make this website. This tag is where I display copywrite. Is it A <footer> or B <copy>?")
+    if(numThree==="A"){
+        alert ("You're ready for more! For the next round, just click on the 4!")
+    }else{
+        let div=document.getElementById ("three").parentElement 
+        let img=document.createElement("img")
+        img.src="pirate.gif"
+        div.append(img)
+        alert ("Oh well...you tried. Today's not your day. The pirate is coming to chase you away! GAME OVER!")
+    }
 } 
            
 
 function frthQuest(){
-     var numFour= prompt("Quest 4 - Solve the riddle: Paragraphs, tables, a form just for kicks. This tag is used to add some nice pics. Is it A <pic> or B <img>?")
-        if(numFour==="B"){
-            alert ("X marks the spot! You're Treasure awaits! You won! Click X before it's too late!")
-        }
-            
-                else alert ("Oh no! You missed it! Your chances are through. The pirate is coming! No treaure for you! GAME OVER!")
+    var numFour= prompt("Quest 4 - Solve the riddle: Paragraphs, tables, a form just for kicks. This tag is used to add some nice pics. Is it A <pic> or B <img>?")
+    if(numFour==="B"){
+        alert ("X marks the spot! You're Treasure awaits! You won! Click X before it's too late!")
+    }else{
+        let div=document.getElementById ("four").parentElement 
+        let img=document.createElement("img")
+        img.src="pirate.gif"
+        div.append(img)
+        alert ("Oh no! You missed it! Your chances are through. The pirate is coming! No treaure for you! GAME OVER!")
+    } 
 }
-/*function fithQuest(){
-     var numFive= prompt("Quest 1 - Solve the riddle: Neat and in order...you get the gist. This tag is used to number your list. Is it A <ol> or B <numli>?")
-        if(numFive==="A"){
-            alert ("You're answer is true! For the next round, just click on the 2!")}
-                
-                else alert ("Oh no! You missed it! No treaure for you!")
-                    }*/
 
 function endQuest(){
     let div=document.getElementById ("finish").parentElement 
@@ -64,9 +68,3 @@ function endQuest(){
     div.append(img)
     alert ("You are the Winner!");
 }
-    /*var goodBye= prompt("Quest 1 - Solve the riddle: Neat and in order...you get the gist. This tag is used to number your list. Is it A <ol> or B <numli>?")
-        if(goodBye==="A"){
-            alert ("You're answer is true! For the next round, just click on the 2!")}
-                                   
-        else alert ("Oh no! You missed it! No treaure for you!")*/
-    
